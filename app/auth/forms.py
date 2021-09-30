61ae56ee5728b3f7eb1d9108008e1db1efda50ed
Myles Spillers <43889349+MSpillers@users.noreply.github.com>
2021-09-30 15:19:08 -0500

    SMD-#09-User-Registration-Basic-Layout
    +Added necessary dependencies
    +Created an initialized database
    +Created Landing Page
    +Created User Registration
    +Added necessary logic for registering user
    +Added Styling
    +Defined logic for App Routes
    +Restructured Folders
    +Added Images

---
+++ app/auth/forms.py
@@ -0,0 +1,43 @@
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField, BooleanField, RadioField, TextAreaField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import InputRequired, DataRequired, EqualTo, Length, ValidationError, Email
+from app.models import User
+
+class RegistrationForm(FlaskForm):
+    username         = StringField("Username *",
+                                validators=[
+                                    InputRequired("Input is required!"),
+                                    DataRequired("Data is required!"),
+                                    Length(min=5, max=20, message="Username must be between 5 and 20 characters long")
+                                ])
+    email            = EmailField("Email *",
+                                validators=[
+                                    InputRequired("Input is required!"),
+                                    DataRequired("Data is required!"),
+                                    Length(min=10, max=30, message="Email must be between 5 and 30 characters long"),
+                                    Email("You did not enter a valid email!")
+                                ])
+    password         = PasswordField("Password *",
+                                validators=[
+                                    InputRequired("Input is required!"),
+                                    DataRequired("Data is required!"),
+                                    Length(min=10, max=40, message="Password must be between 10 and 40 characters long"),
+                                    EqualTo("password_confirm", message="Passwords must match")
+                                ])
+    password_confirm = PasswordField("Confirm Password *",
+                                validators=[
+                                    InputRequired("Input is required!"),
+                                    DataRequired("Data is required!")
+                                ])
+    submit           = SubmitField("Register")
+
+    def validate_username(form, field):
+        user = User.query.filter_by(username=field.data).first()
+        if user:
+            raise ValidationError("Username already exists.")
+
+    def validate_email(form, field):
+        user = User.query.filter_by(email=field.data).first()
+        if user:
+            raise ValidationError("Email already exists.")
