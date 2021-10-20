d41fa39213191df7bc93ad44b1a28ca18f6e1c81
Myles Spillers <43889349+MSpillers@users.noreply.github.com>
2021-10-20 09:03:11 -0500

    SMD-#28-User-Search-Functionality
    +Added Form for Search Functionality
    +Added Filtering Logic to View Page
    -Deleted Old Migration
    -Deleted Unique Constraint in db Causing Error When Sending More Than One Email
    -Removed Unique Constraint from Models

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, session,g
+from flask import Blueprint, render_template, session,g,request
 from app import db, email
+from app.main.forms import SearchForm
 from app.models import Email,User
 from werkzeug.local import LocalProxy
 
@@ -10,16 +11,24 @@ current_user = LocalProxy(lambda: get_current_user())
 
 @main.route('/')
 def home():
-	
+
+	form = SearchForm(request.args,meta={"csrf": False})
 	user_id  = session.get("user_id")
-	print(type(user_id))
-	#user_email=  User.query(email).filter_by(id=user_id)
 	user = db.session.query(User).filter_by(id=user_id).all()
 	user_email = ""
 	for row in user:
 		user_email = row.email
 	emails = db.session.query(Email).filter_by(recipient=user_email)
-	return render_template('home.html', emails=emails)
+	if form.validate():
+		if form.subject.data.strip():
+			search_subject  = form.subject.data
+			search_subject = "%" + search_subject +"%"
+			print(type(search_subject))
+			emails = db.session.query(Email).filter(Email.subject.like(search_subject),Email.uid.like(user_id)).all()
+			print(emails)
+
+	
+	return render_template('home.html', emails=emails,form=form)
 
 
 
