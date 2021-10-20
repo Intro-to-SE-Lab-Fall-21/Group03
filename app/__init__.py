e8c6d5ff24c1bfd35f0bf2f348cb66ef07f79aa0
Myles Spillers <43889349+MSpillers@users.noreply.github.com>
2021-10-20 09:10:46 -0500

    SMD-#2-User-Search-Functionality (#32)
    * SMD-#09- Basic Login Screen
    
    +Added Imports for Flask
    +Added Basic template for login screen
    +Defined App routes
    
    * SMD-#09- Basic Login Screen
    
    +Added Imports for Flask
    +Added Basic template for login screen
    +Defined App routes
    
    * SMD-#09-User-Registration-Basic-Layout
    
    +Added necessary dependencies
    +Created an initialized database
    +Created Landing Page
    +Created User Registration
    +Added necessary logic for registering user
    +Added Styling
    +Defined logic for App Routes
    +Restructured Folders
    +Added Images
    
    * SMD-#23-User-Login-Functionality
    
    +Added  g variable for global current user context
    +Added logic for login and session data
    *Added pages for login
    +Added logic for redirection after login
    +Added Login Form template
    +Added authentication tracking functions
    -Deleted old db migration
    +Added column for password hashes in db
    
    * SMD-#19-#25- Design-Homepage-Email-Composition-Functionality
    
    +Created New Composition Form for Email Creation
    +Added Route For to Composition Page
    +Updated home.html To Return List of User Emails Using Session Info
    +Created  New Migration for db Including Emails Table
    -Removed Old Migration
    
    * SMD-#28-User-Search-Functionality
    
    +Added Form for Search Functionality
    +Added Filtering Logic to View Page
    -Deleted Old Migration
    -Deleted Unique Constraint in db Causing Error When Sending More Than One Email
    -Removed Unique Constraint from Models
    
    * Updated Views.py
    
    -Removed Print Statements

---
+++ app/__init__.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY=os.getenv("FLASK_SECRET_KEY") or 'prc9FWjeLYh_KsPGm0vJcg',
+        SQLALCHEMY_DATABASE_URI='sqlite:///'+ os.path.join(basedir, 'snailmail.sqlite'),
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        DEBUG=True
+    )
+
+    db.init_app(app)
+
+    migrate = Migrate(app,db)
+
+    from app.auth.views import auth
+    from app.main.views import main
+    from app.email.views import email
+    app.register_blueprint(auth)
+    app.register_blueprint(main)
+    app.register_blueprint(email)
+
+    from app.main.errors import page_not_found
+    app.register_error_handler(404, page_not_found)
+
+    return app
